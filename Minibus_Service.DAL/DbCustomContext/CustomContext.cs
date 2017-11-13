844cc820ca259e2cde5aeee7db8f3566e9630d88
andrei <accs.shershen@gmail.com>
2017-11-13 22:25:00 +0300

    DAL - Context
    Create DBContext
    Create DBInitializer

---
+++ Minibus_Service.DAL/DbCustomContext/CustomContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Minibus_Service.DAL.Models;
+
+namespace Minibus_Service.DAL.DbCustomContext
+{
+    class CustomContext:DbContext
+    {
+        public CustomContext(string connectionString)
+            :base(connectionString)
+        {
+            Database.SetInitializer<CustomContext>(new DbInitializer());
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Voyage> Voyages { get; set; }
+    }
+}
---
+++ Minibus_Service.DAL/DbCustomContext/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Minibus_Service.DAL.DbCustomContext
+{
+    class DbInitializer:DropCreateDatabaseIfModelChanges<DbCustomContext.CustomContext>
+    {
+        protected override void Seed(DbCustomContext.CustomContext context)
+        {
+            context.Roles.Add(new Models.Role
+            {
+                RoleName = "operator"
+            });
+
+            context.Roles.Add(new Models.Role
+            {
+                RoleName = "driver"
+            });
+
+            context.SaveChanges();
+
+            var roleId = context.Roles.FirstAsync(r => r.RoleName == "operator").Id;
+
+            context.Users.Add(new Models.User
+            {
+                Login = "Operator",
+                Password = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3", //sha hash
+                RoleId = roleId
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
